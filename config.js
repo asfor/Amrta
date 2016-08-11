6217e6ff8f28ac846e7eca82998a5061fcab721f
asfor <445087813@qq.com>
2016-08-11 12:09:29 +0800

    v0.1.0

--- config.js
+++ config.js
@@ -3,7 +3,15 @@ var config = {
 	defaultController: 'index',		// MVC模式下的默认控制器
 	defaultAction: 'default',		// MVC模式下的默认方法
 	max_MB: 1,						// 上传文件限制大小, 单位MB
-	sessionExpires: 10				// session过期时间, 单位分钟
+	sessionExpires: 10,				// session过期时间, 单位分钟
+	staticType: ['js',				// 支持的静态文件类型
+				 'css',
+				 'jpg',
+				 'jpeg',
+				 'png',
+				 'bmp',
+				 'gif',
+				 'ico']
 };
 
 module.exports = config;
